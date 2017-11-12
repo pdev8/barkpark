f52b311684c59b7234763add7bf29e890fa203e5
Michelle Lee <mhlee3721@gmail.com>
2017-11-11 19:25:35 -0800

    Hardcoded things to map

--- Web/Scripts/Controllers/MapController.js
+++ Web/Scripts/Controllers/MapController.js
@@ -4,12 +4,32 @@
     console.log("in component");
     angular.module(BarkPark).component("mapComponent",
         {
-            //bindings: { addressModel: '<' },
-            //templateUrl: "~/Views/Member/MapTemplate.html",
             templateUrl: "/Scripts/Views/MapTemplate.html",
             controller: "mapController"
         });
 })();
+
+
+// custom directive for submit upon enter
+(function () {
+    'use strict';
+
+    angular
+        .module(BarkPark)
+        .directive('enterKey', function () {
+            return function (scope, element, attrs) {
+                element.bind("keydown keypress", function (event) {
+                    if (event.which === 13) {
+                        scope.$apply(function () {
+                            scope.$eval(attrs.enterKey);
+                        });
+                        event.preventDefault();
+                    };
+                });
+            };
+        });
+})();
+
 
 
 // the map controller
@@ -31,30 +51,23 @@
 
         vm.$onInit = _initMap;
 
-        
+
 
         function _initMap() {
 
+            // upon page load, show map centered on Riverside area
             var map = new google.maps.Map(document.getElementById('gmap'), {
-                center: { lat: -34.397, lng: 150.644 },
-                zoom: 8
+                //center: { lat: 39.8283, lng: -98.5795 },
+                center: { lat: 33.97, lng: -117.3281 },
+                zoom: 9
             });
 
-            //var userMap = new google.maps.Map(document.getElementById("gmap"), {
-            //    zoom: 8,
-            //    center: ()
-            //});
-
-
-            //vm.mapObj = {
-            //    'address': address,
-            //    'map': userMap
-            //}
 
         }
 
-   
+
         function _submitAddress() {
+            console.log("Submitting address");
             // recenter map around the user-input zipcode (upon click of submit)
 
             // store the user input postal code as variable address
@@ -72,135 +85,96 @@
 
                     // need to re-instantiate the map here because... not sure exactly why; something to do with google api ?
                     var userMap = new google.maps.Map(document.getElementById("gmap"), {
-                        zoom: 8,
+                        zoom: 12,
                         center: (results[0].geometry.location)
                     });
                     vm.mapObj = {
                         'address': address,
                         'map': userMap
                     }
-                    _getEvents(vm.mapObj);
+                    //_getEvents(vm.mapObj);
+
+                    var hardCodedArray = [
+                        { latitude: 34.0100, longitude: -118.4961, locationName: "Santa Monica Pier", eventDescription: "A popular beach area that allows for leashed dogs. Easy parking.", count: "10", averageRating: 4, totalRatings: 10, fullAddress: "1846 W Linden St, Riverside, CA 92507" },
+                        { latitude: 34.075207, longitude: -118.41885, locationName: "Whittier Dog Park", eventDescription: "A small area within a country club. Easy parking.", count: "4", averageRating: 4, totalRatings: 10, fullAddress: "1846 W Linden St, Riverside, CA 92507" },
+                        { latitude: 34.006367, longitude: -118.46567, locationName: "Marine Park", eventDescription: "A city dog park with plenty of benches.", count: "15", averageRating: 4, totalRatings: 10, fullAddress: "1846 W Linden St, Riverside, CA 92507" }
+                        //{ latitude: 34.0100, longitude: -118.4961, locationName: "Beach", eventDescription: "A popular beach that allows for leashed dogs. Easy parking.", count: "10", averageRating: 4, totalRatings: 10, fullAddress: "1846 W Linden St, Riverside, CA 92507" },
+                        //{ latitude: 34.0100, longitude: -118.4961, locationName: "Beach", eventDescription: "A popular beach that allows for leashed dogs. Easy parking.", count: "10", averageRating: 4, totalRatings: 10, fullAddress: "1846 W Linden St, Riverside, CA 92507" }
+                    ];
+                    _populateMap(hardCodedArray);
                 } else {
                     console.log('ERROR map not recentering');
                 }
             });
+
+
+            // populate the LIST view
+
+
         }
 
-        // get the events/locations within the appropriate radius; drop corresponding markers on map
-        function _getEvents(mapObj) {
-            mapsService.GetEvents(mapObj.address)
-                .then(function (data) {
-                    // this data is an object, that contains a property that's an array full of events based off of the zip
-                    console.log('HEY LOOK HERE!!!!!!!!!!*********************: ', data);
-                    // call the _populateMap method to iterate through this array of addresses and populate them on the mmap
-                    _populateMap(data);
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-        }
-
-
-        function _populateMap(data) {
+        function _populateMap(hardCodedArray) {
             // conditional statement to check if there is even anything returned; i.e. if there are even any locations that need to be marked on the map
-            if (data.items) {
-                // an array of addresses is returned; store in var addrArr
-                var addrArr = data.items;
+            // an array of addresses is returned; store in var addrArr
+            var addrArr = hardCodedArray;
 
-                console.log('data.items in _populateMap: ', data.items);
-                
-                for (var i = 0; i < addrArr.length; i++) {
+            for (var i = 0; i < addrArr.length; i++) {
 
-                    var currentIteration = addrArr[i];
-                    console.log('currentIteration: ', currentIteration);
-                    var myLatLng = {
-                        'lat': currentIteration.latitude,
-                        'lng': currentIteration.longitude
-                    };
-                    console.log('myLatLng: ', myLatLng);
+                var currentIteration = addrArr[i];
+                console.log('currentIteration: ', currentIteration);
+                var myLatLng = {
+                    'lat': currentIteration.latitude,
+                    'lng': currentIteration.longitude
+                };
 
-                    // depending on the averageRating of the particular location, change the number of stars displayed as filled (the corresponding html is included in windowContent below)
-                    if (currentIteration.averageRating < .75) {
-                        var ratingStars =
-                            '<i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i>';
-                    } else if (currentIteration.averageRating > .75 &&
-                        currentIteration.averageRating < 1.75) {
-                        var ratingStars =
-                            '<i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i>';
-                    } else if (currentIteration.averageRating > 1.75 &&
-                        currentIteration.averageRating < 2.75) {
-                        var ratingStars =
-                            '<i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i>';
-                    } else if (currentIteration.averageRating > 2.75 &&
-                        currentIteration.averageRating < 3.75) {
-                        var ratingStars =
-                            '<i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i>';
-                    } else if (currentIteration.averageRating > 3.75 &&
-                        currentIteration.averageRating < 4.75) {
-                        var ratingStars =
-                            '<i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i>';
-                    } else if (currentIteration.averageRating > 4.75) {
-                        var ratingStars =
-                            '<i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i>';
-                    }
+                // the actual HTML content of the little popup box for each marker, stored in var boxContent
+                var windowContent = '<div id="content">' +
+                    '<div id="siteNotice">' +
+                    '</div>' +
+                    '<div><h3 id="firstHeading" style="color:#8dc344;" class="firstHeading">' +
+                    currentIteration.locationName +
+                    //'</h3><h5 style="color:#228B22;">' +
+                    //'<i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star"></i><i style="color:#228B22;" class="glyphicon glyphicon-star-empty"></i>'; +
+                    ' ' +
+                    '<h5><strong>People: </strong> ' +
+                    currentIteration.count +
+                    '<div id="bodyContent">' +
+                    '</h5><h5>' +
+                    currentIteration.eventDescription +
+                    '<h5><strong>Address: </strong> ' +
+                    currentIteration.fullAddress +
+                    '</h5>';
 
-                    // the actual HTML content of the little popup box for each marker, stored in var boxContent
-                    var windowContent = '<div id="content">' +
-                        '<div id="siteNotice">' +
-                        '</div>' +
-                        '<div><h3 id="firstHeading" style="color:#8dc344;" class="firstHeading">' +
-                        currentIteration.typeName +
-                        '</h3><h5 style="color:#228B22;">' +
-                        ratingStars +
-                        ' ' +
-                        currentIteration.totalRatings +
-                        '</h5>' +
-                        '<div id="bodyContent">' +
-                        '<h5><strong>Address: </strong> ' +
-                        currentIteration.fullAddress +
-                        '</h5><h5><strong>Description: </strong>' +
-                        currentIteration.eventDescription +
-                        '</h5>';
+                // assign default marker color
+                var markerColor = "ff0000";
 
-                    // assign default marker color
-                    var markerColor = "228B22";
+                // using the selected marker color, store marker image as var formattedMarkerIcon to be used as property of Marker object below
+                var formattedMarkerIcon = new google.maps.MarkerImage(
+                    "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" +
+                    markerColor);
 
-                    // if the location is of type 'Location', change marker to custom color
-                    if (currentIteration.maptype === 'Location') {
-                        markerColor = "90FB75";
-                    }
+                var infoWindow = new google.maps.InfoWindow();
 
-                    // using the selected marker color, store marker image as var formattedMarkerIcon to be used as property of Marker object below
-                    var formattedMarkerIcon = new google.maps.MarkerImage(
-                        "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" +
-                        markerColor);
+                // new marker object with properties that we've defined above
+                var marker = new google.maps.Marker({
+                    map: vm.mapObj.map,
+                    position: myLatLng,
+                    icon: formattedMarkerIcon
+                });
 
-                    var infoWindow = new google.maps.InfoWindow();
 
-                    // new marker object with properties that we've defined above
-                    var marker = new google.maps.Marker({
-                        map: vm.mapObj.map,
-                        position: myLatLng,
-                        title: currentIteration.typeName,
-                        icon: formattedMarkerIcon
+                marker.addListener('click',
+                    function () {
+                        // check to see if there is already an info window open; if so, close it before proceeding
+                        if (vm.prevInfoWindow) {
+                            vm.prevInfoWindow.close();
+                        }
+
+                        // update prevInfoWindow to this new instance, set content, and open it on the map 'map' anchored on marker 'marker'
+                        vm.prevInfoWindow = infoWindow;
+                        infoWindow.setContent(windowContent);
+                        infoWindow.open(map, marker);
                     });
-
-
-                    marker.addListener('click',
-                        function () {
-                            // check to see if there is already an info window open; if so, close it before proceeding
-                            if (vm.prevInfoWindow) {
-                                vm.prevInfoWindow.close();
-                            }
-
-                            // update prevInfoWindow to this new instance, set content, and open it on the map 'map' anchored on marker 'marker'
-                            vm.prevInfoWindow = infoWindow;
-                            infoWindow.setContent(windowContent);
-                            infoWindow.open(map, marker);
-                        });
-
-
-                }
 
 
             };
