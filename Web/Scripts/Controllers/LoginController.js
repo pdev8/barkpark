da6b3b82fba5086a7cfec70004e41145b0bf6f85
Paul Lee <ppauleee@gmail.com>
2017-11-11 17:31:24 -0800

    Added login controller

---
+++ Web/Scripts/Controllers/LoginController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    angular.module(BarkPark).controller("loginController", LoginController);
+
+    LoginController.$inject = ["$scope"];
+
+    function LoginController($scope) {
+        var vm = this;
+
+        vm.login = _login;
+
+        vm.$onInit = _init;
+
+        function _login() {
+            location.href = "Member";
+        }
+    }
+})();
